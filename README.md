1ede5b9dbeaf7a3747b7944f7f7d3d8602802e00
Anne Blankert <anne.blankert@geodan.nl>
2020-09-14 21:01:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# pgsql2mvt
+Serve user defined PostGIS queries as mapbox vector tile layers  
+Other mvt servers let you define databases, fields and tables, but if you have special requirements, such as multiple layer output in a single mvt tile or special 'where' clauses or joined tables or unions or function calls or type conversions or concatenated attributes or tables from multiple databases, this tool may come in handy.
+
+## Requirements
+* git
+* node
+
+## install
+```
+git clone this_repository
+cd this_repository
+npm install
+```
+
+## configure
+The pgsql2mvt service requires one json configuration file per layer in directory 'layers'  
+See layers/example.json.example, remove the '.example' extension and edit to your needs. ${z}, ${x} and ${y} are placeholders for tile coordinates z, x, y
+
+Restart the service to load configuration changes
+
+**Note**: for reasons of user-friendlyness, the sql queries in the layer configuration can be split into multiple json lines (json array of strings). If you copy-paste these lines into tool '[DBeaver](https://dbeaver.io)', DBeaver automagically removes quotes and trailing comma's and asks to supply values for z, x and y. You can inspect the generated sql query by setting .sql as extension (see urls described below)
+
+## run
+```
+npm start
+```
+
+The started service serves tiles at urls: 
+1. http://localhost:8095/mvt/example/z/x/y.mvt
+2. http://localhost:8095/mvt/example/z/x/y.pbf
+
+Where 'example' is the name of your layer (as defined in layers/example.json)   
+z, x, y, are the tile coordinates for the mvt or pbf tile. The contents of mvt and pbf are identical.
+
+You can inspect the used sql query at url (config.json property 'sqlinfo': true):
+http://localhost:8095/mvt/example/z/x/y.sql
+
---
+++ pgsql2mvt.js
@@ -0,0 +1,115 @@
+const serverconfig = require('./config.json')
+
+const fs = require('fs');
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(logger(':date[iso] ":method :url"'));
+app.use(cors());
+
+const pgp = require('pg-promise')({
+    /*query: (e) => {
+        console.log(e.query);
+    }*/
+});
+
+function loadLayerMap(){
+    // assumes queries are in subdirectory 'layers'
+    let files = fs.readdirSync(`${__dirname}/layers`);
+    files = files.filter(file=>file.slice(-5) === '.json');
+    let configs = files.map(file=>{
+        let rawdata = fs.readFileSync(`${__dirname}/layers/${file}`);
+        let config = JSON.parse(rawdata);
+        config.host = config.host ? config.host : process.env.PGHOST;
+        config.user = config.user ? config.user : process.env.PGUSER;
+        config.password = config.password ? config.password : process.PGPASSWORD;
+        config.database = config.database ? config.database : process.env.PGDATABASE;
+        config.ssl = config.ssl ? config.ssl : process.PGSSLMODE;
+        config.port = config.port ? config.port : process.PGPORT ? process.env.PGPORT : 5432;
+        if (Array.isArray(config.sql)) {
+            config.sql = config.sql.join('\n');
+        }    
+        config.file = file;
+        config.layer = file.slice(0,-5);
+        config.dbConnection = pgp(config);
+        return config;
+    });
+    let layerMap = new Map();
+    for (let config of configs) {
+        layerMap.set(config.layer, config);
+    }
+    return layerMap;
+}
+
+let layerMap = loadLayerMap();
+
+app.get('/mvt/:layer/:z/:x/:y.:extension', async (req, res)=> {
+    let config = layerMap.get(req.params.layer);
+    if (!config) {
+        res.status(422).json({error: `layer '${req.params.layer}' not found`})
+        return;
+    }
+    let z = parseInt(req.params.z);
+    let x = parseInt(req.params.x);
+    let y = parseInt(req.params.y);
+    let sql = config.sql.replace(/\${z}|\${x}|\${y}/gi, (match)=>{
+        switch (match) {
+            case '${z}':
+            case '${Z}':
+                return z.toString();
+            case '${x}':
+            case '${X}':
+                return x.toString();
+            case '${y}':
+            case '${Y}':
+                return y.toString();
+            default:
+                return 'undefined'
+        }
+    });
+    switch(req.params.extension) {
+        case 'sql':
+            if (serverconfig.sqlinfo) {
+                res.json({sql: sql});
+            } else {
+                res.status('403').json({error: 'sql info not enabled'})
+            }
+            break;
+        case 'mvt':
+        case 'pbf':
+            try {
+                const result = await config.dbConnection.one(sql, []);
+                const tileData = result.tile ? result.tile : result[Object.keys(result)[0]];
+                if (tileData.length === 0) {
+                    res.status(204)
+                }
+                res.header('Content-Type', 'application/x-protobuf').send(tileData);
+            } catch(err) {
+                console.log(err);
+                let status = 500;
+                switch (err.code) {
+                    case '42P01':
+                        // table does not exist
+                        status = 422;
+                        break;
+                    case '42703':
+                        // column does not exist
+                        status = 422;
+                        break;
+                    default:
+                }
+                res.status(status).json({error:err.message})
+            }
+            break;
+        default:
+            res.status(422).json({error: `extension '${req.params.extension}' not supported`})
+    }
+})
+
+const server = app.listen(serverconfig.port);
+server.setTimeout(600000);
+console.log(`pgserver listening on port ${serverconfig.port}`);
+
